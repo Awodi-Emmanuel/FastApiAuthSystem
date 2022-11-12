56dded741e703a9e2582d7c9586e35c467850ed7
Awodi-Emmanuel <Emmanuelawodi25@gmail.com>
2022-11-12 15:37:00 +0100

    connect cosmodb to fastapi sucessfully

--- manage.py
+++ manage.py
@@ -16,3 +16,6 @@ async def startup_db_client():
     app.cosmos_client = CosmosClient(config["URL"], credential = config["KEY"])
     await get_or_create_db(DATABASE_NAME)
     await get_or_create_container(CONTAINER_NAME)
+
+async def get_or_create_db(db_name):
+    
