4746c694375e4cf0b479658e748cd0d631883700
Awodi-Emmanuel <Emmanuelawodi25@gmail.com>
2022-11-12 15:31:03 +0100

    connect cosmodb to fastapi sucessfully

---
+++ manage.py
@@ -0,0 +1,18 @@
+from fastapi import FastAPI
+from dotenv import dotenv_values
+from azure.cosmos.aio import CosmosClient
+from azure.cosmos import PartitionKey, exceptions
+
+
+# Load the credentials in config variable, instantiate FastAPI, and define Database and Container name.
+
+config = dotenv_values(".env")
+app = FastAPI()
+DATABASE_NAME = "todo-bd"
+CONTAINER_NAME = "todo_items"
+
+@app.on_envent('startup')
+async def startup_db_client():
+    app.cosmos_client = CosmosClient(config["URL"], credential = config["KEY"])
+    await get_or_create_db(DATABASE_NAME)
+    await get_or_create_container(CONTAINER_NAME)
